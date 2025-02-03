0b174e8810bc8adfa5180c69cad143e1442ec4a8
DESKTOP-RIMIB5Q\jpribio <jaime.p.r023@gmail.com>
2025-02-03 14:43:00 +0100

    inicio sesion sin terminar

--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio-sesion.component.html',
-  styleUrl: './inicio-sesion.component.css'
+  styleUrl: './inicio-sesion.component.css',
 })
 export class InicioSesionComponent {
+  isFlipped: boolean = false;
+  isSignUpActive: boolean = false; // Controla el estado de la vista
 
+  toggleForm(): void {
+    this.isSignUpActive = !this.isSignUpActive;
+    console.log(this.isSignUpActive);
+    
+  }
+  flipForm(): void {
+    this.isFlipped = !this.isFlipped;
+  }
 }
